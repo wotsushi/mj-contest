11d5774a559ca07a96d70e7c3b68243f8bdd7064
wotsushi <wotsushi@gmail.com>
2025-03-08 14:53:20 +0900

    結果入力 持ち点入力・ポイント表示

--- src/Form/Table.tsx
+++ src/Form/Table.tsx
@@ -1,4 +1,24 @@
+import { ChangeEventHandler, useState } from "react";
+import { calcPoints } from "../point";
+
 const Table: React.FC = () => {
+  const [scores, setScores] = useState<(number | null)[]>([
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const onChangeScore =
+    (i: number): ChangeEventHandler<HTMLInputElement> =>
+    (e) => {
+      const newScores = [...scores];
+      newScores[i] = parseInt(e.target.value, 10);
+      setScores(newScores);
+    };
+  const points =
+    scores.some((score) => score === null || isNaN(score)) ?
+      scores.map(() => null)
+    : calcPoints(scores.filter((s) => s !== null));
   return (
     <table>
       <thead>
@@ -11,16 +31,21 @@ const Table: React.FC = () => {
       </thead>
       <tbody>
         <tr>
-          <td>40,000</td>
-          <td>30,000</td>
-          <td>20,000</td>
-          <td>10,000</td>
+          {scores.map((score, i) => (
+            <td key={i}>
+              <input
+                type="number"
+                step="100"
+                value={score ?? ""}
+                onChange={onChangeScore(i)}
+              />
+            </td>
+          ))}
         </tr>
         <tr>
-          <td>40.0</td>
-          <td>5.0</td>
-          <td>-15.0</td>
-          <td>-30.0</td>
+          {points.map((point, i) => (
+            <td key={i}>{point?.toFixed(1) ?? ""}</td>
+          ))}
         </tr>
       </tbody>
     </table>
