c5bcfdad36d77a10ad7fb71a9996ec753d04226d
wotsushi <wotsushi@gmail.com>
2025-03-13 00:09:12 +0900

    大会編集画面 簡易版

---
+++ src/EditContest/index.tsx
@@ -0,0 +1,76 @@
+import { Docs } from "../firebase";
+import { useParams } from "react-router";
+
+const EditContest: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [contest, mutate] = Docs.useContest(id ?? "");
+  if (id === undefined || contest === null) return null;
+  return (
+    <div>
+      {contest.results.map((results, i) => (
+        <div key={i}>
+          <div>{i + 1}回戦</div>
+          {results.map((result, j) => (
+            <div key={j}>
+              <div>
+                <input
+                  type="text"
+                  value={result.table}
+                  size={10}
+                  onChange={(e) =>
+                    mutate((next) => {
+                      next.results[i][j].table = e.target.value;
+                    })
+                  }
+                />
+              </div>
+              <div>
+                {result.players.map((player, k) => (
+                  <input
+                    key={k}
+                    min={0}
+                    max={100}
+                    type="number"
+                    value={player}
+                    onChange={(e) =>
+                      mutate((next) => {
+                        next.results[i][j].players[k] = parseInt(
+                          e.target.value,
+                        );
+                      })
+                    }
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+          <button
+            type="button"
+            onClick={() =>
+              mutate((next) =>
+                next.results[i].push({
+                  table: "",
+                  players: [0, 0, 0, 0],
+                  scores: null,
+                }),
+              )
+            }
+          >
+            卓追加
+          </button>
+        </div>
+      ))}
+      <button
+        type="button"
+        onClick={() => mutate((next) => next.results.push([]))}
+      >
+        追加
+      </button>
+      <button type="button" onClick={() => Docs.saveContest(id, contest)}>
+        保存
+      </button>
+    </div>
+  );
+};
+
+export default EditContest;
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes } from "react-router";
 import { Route } from "react-router";
 import Result from "./Result.tsx";
 import Form from "./Form";
+import EditContest from "./EditContest/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />} />
         <Route path="/result" element={<Result />} />
         <Route path="/form" element={<Form />} />
+        <Route path="/edit_contest/:id" element={<EditContest />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
