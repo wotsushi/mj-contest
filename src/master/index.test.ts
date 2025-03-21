5b3a54e40affe43620147f95fb9ab28f660b3d7a
wotsushi <wotsushi@gmail.com>
2025-03-21 10:53:20 +0900

    テスト追加 useMaster

---
+++ src/master/index.test.ts
@@ -0,0 +1,38 @@
+import { act, renderHook } from "@testing-library/react";
+import { useMaster } from ".";
+import { mockOnSnapshot } from "../firebase/testing";
+
+describe("useMaster", () => {
+  describe("nameByID", () => {
+    type TestCase = [id: number, name: string][];
+    it.each<TestCase>([
+      [
+        [1, "subaru"],
+        [42, ""],
+      ],
+      [
+        [1, "subaru"],
+        [42, "subaru"],
+      ],
+      [],
+    ])(`players=%p`, (...players) => {
+      const sendSnapshot = mockOnSnapshot();
+      const { result } = renderHook(() => useMaster());
+      act(() =>
+        sendSnapshot.call({
+          players: Object.fromEntries(
+            players.map(([id, name], i) => [
+              i,
+              {
+                id,
+                name,
+              },
+            ]),
+          ),
+          rules: [],
+        }),
+      );
+      expect(result.current.nameByID).toEqual(new Map(players));
+    });
+  });
+});
--- src/master/index.ts
+++ src/master/index.ts
@@ -1,4 +1,10 @@
-import { useDoc } from "./firebase";
+import { useDoc } from "../firebase";
+
+export type Master = { players: Player[]; rules: Rule[] };
+
+export type Player = { id: number; name: string };
+
+type Rule = never;
 
 export const useMaster = () => {
   const { state: master } = useDoc<Master>("master");
@@ -6,19 +12,5 @@ export const useMaster = () => {
     master === null ? null : (
       new Map(master.players.map(({ id, name }) => [id, name]))
     );
-  return {
-    nameByID,
-  };
+  return { nameByID };
 };
-
-type Master = {
-  players: Player[];
-  rules: Rule[];
-};
-
-type Player = {
-  id: number;
-  name: string;
-};
-
-type Rule = never;
