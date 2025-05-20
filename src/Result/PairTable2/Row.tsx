08a997d3e13f00ceb2bb8da3720db53d728853ff
wotsushi <wotsushi@gmail.com>
2025-05-20 21:00:47 +0900

    結果表 選手ごとの合計も表示できるようにした

---
+++ src/Result/PairTable2/Row.tsx
@@ -0,0 +1,113 @@
+import styled from "styled-components";
+
+type Props = {
+  rank: number;
+  names: [string | undefined, string | undefined];
+  total: number;
+  subtotal: [number, number];
+  points: [(number | string)[], (number | string)[]];
+};
+
+const Row: React.FC<Props> = ({ rank, names, total, subtotal, points }) => {
+  return (
+    <>
+      <Tr>
+        <Rank $rank={rank} rowSpan={2}>
+          {rank}
+        </Rank>
+        <Name>{names[0]}</Name>
+        <Total rowSpan={2}>
+          <NumberLabel $negative={total < 0}>{total.toFixed(1)}</NumberLabel>
+        </Total>
+        <SubTotal>
+          <NumberLabel $negative={subtotal[0] < 0}>
+            {subtotal[0].toFixed(1)}
+          </NumberLabel>
+        </SubTotal>
+        {points[0].map((point, i) => {
+          if (typeof point === "string") {
+            return (
+              <Cell key={i}>
+                <NumberLabel>{point}</NumberLabel>
+              </Cell>
+            );
+          }
+          return (
+            <Cell key={i}>
+              <NumberLabel $negative={point < 0}>
+                {point.toFixed(1)}
+              </NumberLabel>
+            </Cell>
+          );
+        })}
+      </Tr>
+      <Tr>
+        <Name>{names[1]}</Name>
+        <SubTotal>
+          <NumberLabel $negative={subtotal[1] < 0}>
+            {subtotal[1].toFixed(1)}
+          </NumberLabel>
+        </SubTotal>
+        {points[1].map((point, i) => {
+          if (typeof point === "string") {
+            return (
+              <Cell key={i}>
+                <NumberLabel>{point}</NumberLabel>
+              </Cell>
+            );
+          }
+          return (
+            <Cell key={i}>
+              <NumberLabel $negative={point < 0}>
+                {point.toFixed(1)}
+              </NumberLabel>
+            </Cell>
+          );
+        })}
+      </Tr>
+    </>
+  );
+};
+
+const Tr = styled.tr`
+  &:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+
+  &:nth-child(odd) {
+    background-color: #fff;
+  }
+`;
+
+const Cell = styled.td`
+  color: #434343;
+`;
+
+const Rank = styled(Cell)<{ $rank: number }>`
+  font-weight: bold;
+  color: ${({ $rank }) => ["#d4af37", "#a9a9a9", "#8b5a2b"].at($rank - 1)};
+`;
+
+const Name = styled(Cell)`
+  background-color: #d0e0e3;
+  border-bottom: 1px solid #f2f2f2;
+`;
+
+const Total = styled(Cell)`
+  font-weight: bold;
+  background-color: #fff2cc;
+  border-bottom: 1px solid #f8e09b;
+`;
+
+const SubTotal = styled(Cell)`
+  background-color: #fafad2;
+  border-bottom: 1px solid #f8e09b;
+`;
+
+const NumberLabel = styled.div<{ $negative?: boolean }>`
+  padding-right: 33%;
+  color: ${({ $negative }) => ($negative ? "#ff0000" : undefined)};
+  text-align: right;
+`;
+
+export default Row;
--- src/Result/index.tsx
+++ src/Result/index.tsx
@@ -1,12 +1,14 @@
 import { calcPoints } from "../point";
 import { useContest } from "../contest";
-import { useParams } from "react-router";
+import { useParams, useSearchParams } from "react-router";
 import { useMaster } from "../master";
 import NormalTable from "./NormalTable";
 import PairTable from "./PairTable";
+import PairTable2 from "./PairTable2";
 
 const Result: React.FC = () => {
   const { id } = useParams<{ id: string }>();
+  const [params] = useSearchParams();
   const { nameByID } = useMaster();
   const { contest } = useContest(id ?? "");
   if (nameByID === null || contest === null) return null;
@@ -23,6 +25,15 @@ const Result: React.FC = () => {
     ]),
   );
   if (contest.rule?.id === "pair") {
+    if (params.has("subtotal")) {
+      return (
+        <PairTable2
+          nameByID={nameByID}
+          pairs={contest.rule.pairs}
+          pointsByID={pointsByID}
+        />
+      );
+    }
     return (
       <PairTable
         nameByID={nameByID}
